0fa600604d5ce6a77dd46ea5c84a1dfbe61197d9
Keshavsingh0811 <keshavsingh39357@gmail.com>
2024-05-18 14:25:05 +0530

    Solent

---
+++ SOLENT HIGHSCHOOL/JwtWebApi/JwtWebApi/JwtWebApiTutorial/User.cs
@@ -0,0 +1,18 @@
+﻿namespace JwtWebApiTutorial
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public string RefreshToken { get; set; } = string.Empty;
+        public DateTime TokenCreated { get; set; }
+        public DateTime TokenExpires { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string Surname { get; set; } = string.Empty;
+        public string MobileNumber { get; set; } = string.Empty;
+        public string Gender { get; set; } = string.Empty;
+        public string Comments { get; set; } = string.Empty;
+    }
+}
---
+++ SOLENT HIGHSCHOOL/JwtWebApi/JwtWebApi/JwtWebApiTutorial/UserDto.cs
@@ -0,0 +1,14 @@
+﻿namespace JwtWebApiTutorial
+{
+    public class UserDto
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string ConfirmPassword { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string Surname { get; set; } = string.Empty;
+        public string MobileNumber { get; set; } = string.Empty;
+        public string Gender { get; set; } = string.Empty;
+        public string Comments { get; set; } = string.Empty;
+    }
+}
